af8c1577cdde1a4ec2c73162d56bd38f5910ca6c
impactrudia <impactyoung@gmail.com>
2021-10-29 16:12:45 +0900

    feat : [검색 화면] 검색 결과 테스트 데이터

---
+++ ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/adapters/SearchMovieAdapter.kt
@@ -0,0 +1,66 @@
+package com.happymoonday.challengesforheymoon.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.happymoonday.challengesforheymoon.R
+import com.happymoonday.challengesforheymoon.databinding.ItemMovieFavoritesBinding
+import com.happymoonday.challengesforheymoon.domain.model.Movie
+import com.happymoonday.challengesforheymoon.presentation.ui.search.ChooseGenreFragmentDirections
+
+class SearchMovieAdapter :
+    ListAdapter<Movie, SearchMovieAdapter.ViewHolder>(ChooseMovieDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_movie_favorites,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: ItemMovieFavoritesBinding
+    ) : RecyclerView.ViewHolder(binding.root){
+        init {
+            binding.setClickListener { view ->
+                navigateToHome(view)
+            }
+        }
+
+        private fun navigateToHome(view: View){
+            val action = ChooseGenreFragmentDirections.actionFragmentChooseGenreToFragmentNation()
+            view.findNavController().navigate(action)
+        }
+
+        fun bind(movie: Movie) {
+            with(binding) {
+                binding.item = movie
+                executePendingBindings()
+            }
+        }
+    }
+
+}
+
+private class ChooseMovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem.title == newItem.title
+    }
+
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
+    }
+}
--- ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/search/SearchViewModel.kt
+++ ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/search/SearchViewModel.kt
@@ -1,8 +1,11 @@
 package com.happymoonday.challengesforheymoon.presentation.ui.search
 
+import androidx.lifecycle.LiveData
+import com.happymoonday.challengesforheymoon.domain.model.Movie
 import com.happymoonday.challengesforheymoon.domain.model.reqeuest.ReqMovie
 import com.happymoonday.challengesforheymoon.presentation.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel() {
-    var movie : ReqMovie? = ReqMovie()
+    var movie: ReqMovie? = ReqMovie()
+    val searchMovies: LiveData<List<Movie>>? = null
 }
