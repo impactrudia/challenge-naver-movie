87a56b8d881bcef4f5393eb40e0b827bc57aba12
impactrudia <impactyoung@gmail.com>
2021-10-29 16:42:25 +0900

    feat : [검색 화면] 검색 결과 테스트 데이터

--- ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/search/SearchResultFragment.kt
+++ ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/search/SearchResultFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import androidx.navigation.navGraphViewModels
 import com.happymoonday.challengesforheymoon.R
+import com.happymoonday.challengesforheymoon.adapters.ChooseNationAdapter
+import com.happymoonday.challengesforheymoon.adapters.SearchMovieAdapter
 import com.happymoonday.challengesforheymoon.databinding.FragmentSearchResultsBinding
+import com.happymoonday.challengesforheymoon.domain.enums.CountryType
+import com.happymoonday.challengesforheymoon.domain.model.Movie
 import com.happymoonday.challengesforheymoon.presentation.base.BaseFragment
 
 class SearchResultFragment : BaseFragment() {
@@ -26,12 +31,35 @@ class SearchResultFragment : BaseFragment() {
             textEmptyMessage.setOnClickListener { //TODO FIX
                 showFavoriteAlert()
             }
+
+            val adapter = SearchMovieAdapter()
+            recyclerView.adapter = adapter
+
+            subscribeUi(adapter)
         }
 
         return binding.root
     }
 
-    fun showFavoriteAlert(){
+    private fun subscribeUi(adapter: SearchMovieAdapter) {
+        val item = Movie(
+            "홈런",
+            "https://movie.naver.com/movie/bi/mi/basic.nhn?code=187310",
+            "https://ssl.pstatic.net/imgmovie/mdi/mit110/1873/187310_P20_100054.jpg",
+        "Minari",
+        "2020",
+        "정이삭|",
+        "윌 패튼|스티븐 연|한예리|윤여정|앨런 김|노엘 조|",
+        "7.58")
+        var items = listOf<Movie>(item, item, item, item)
+        adapter.submitList(items)
+
+        //TODO FIX
+        binding.recyclerView.isVisible = !items.isNullOrEmpty()
+        binding.textEmptyMessage.isVisible = items.isNullOrEmpty()
+    }
+
+    private fun showFavoriteAlert(){
         val alertDialog: AlertDialog? = this.let {
             val builder = AlertDialog.Builder(requireContext())
             builder.apply {
@@ -49,4 +77,5 @@ class SearchResultFragment : BaseFragment() {
         }
         alertDialog?.show()
     }
+
 }
