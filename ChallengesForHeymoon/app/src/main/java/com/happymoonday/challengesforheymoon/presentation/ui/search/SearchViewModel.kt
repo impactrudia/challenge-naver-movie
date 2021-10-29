c391008dcdd159a33d2cbdd9285514ecb2a9a902
impactrudia <impactyoung@gmail.com>
2021-10-29 16:13:31 +0900

    Revert "feat : [검색 화면] 검색 결과 테스트 데이터"
    This reverts commit af8c1577cdde1a4ec2c73162d56bd38f5910ca6c.

--- ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/search/SearchViewModel.kt
+++ ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/search/SearchViewModel.kt
@@ -1,11 +1,8 @@
 package com.happymoonday.challengesforheymoon.presentation.ui.search
 
-import androidx.lifecycle.LiveData
-import com.happymoonday.challengesforheymoon.domain.model.Movie
 import com.happymoonday.challengesforheymoon.domain.model.reqeuest.ReqMovie
 import com.happymoonday.challengesforheymoon.presentation.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel() {
-    var movie: ReqMovie? = ReqMovie()
-    val searchMovies: LiveData<List<Movie>>? = null
+    var movie : ReqMovie? = ReqMovie()
 }
