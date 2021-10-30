985cccee56b05a40e77b172d0a811b3a34ba4578
impactrudia <impactyoung@gmail.com>
2021-10-30 16:30:32 +0900

    feat : [검색 화면] 즐겨찾기 추가 후 즐겨찾기 탭으로 이동

--- ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/domain/constants/Constants.kt
+++ ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/domain/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.happymoonday.challengesforheymoon.domain.constants
 
 object Constants {
     const val MOVIE_DB = "movie-db"
-    const val REQUEST_MOVIE_SEARCH = 0x001
+
     const val BUNDLE_KEYWORD = "BUNDLE_KEYWORD"
     const val BUNDLE_LINK = "BUNDLE_LINK"
 }
--- ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/home/MovieSearchFragment.kt
+++ ChallengesForHeymoon/app/src/main/java/com/happymoonday/challengesforheymoon/presentation/ui/home/MovieSearchFragment.kt
@@ -1,11 +1,17 @@
 package com.happymoonday.challengesforheymoon.presentation.ui.home
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.viewpager2.widget.ViewPager2
+import com.happymoonday.challengesforheymoon.R
+import com.happymoonday.challengesforheymoon.adapters.MOVIE_FAVORITES_PAGE_INDEX
 import com.happymoonday.challengesforheymoon.databinding.FragmentMovieSearchBinding
 import com.happymoonday.challengesforheymoon.domain.constants.Constants
 import com.happymoonday.challengesforheymoon.presentation.ui.search.SearchActivity
@@ -23,10 +29,16 @@ class MovieSearchFragment : Fragment() {
         binding.btnSearchKeyword.setOnClickListener {
             val intent = Intent(requireContext(), SearchActivity::class.java)
             intent.putExtra(Constants.BUNDLE_KEYWORD, binding.editInputSearch.text)
-            startActivityForResult(intent, Constants.REQUEST_MOVIE_SEARCH)
+            startForResult.launch(intent)
         }
 
         return binding.root
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            requireActivity().findViewById<ViewPager2>(R.id.viewPager).currentItem = MOVIE_FAVORITES_PAGE_INDEX
+        }
     }
 
 }
